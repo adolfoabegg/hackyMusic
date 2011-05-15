dc6c590cf38a6d185ce31b6d996ea6d4a0bbbd5e
motanelu <miau@motane.lu>
2011-05-15 04:15:09 +0300

    Field name fix

--- public/js/yHack.js
+++ public/js/yHack.js
@@ -1,21 +1,21 @@
 yHack = {
 	init : function() {
 		if (navigator.geolocation) {
-			$('input[name=searchbox]').val('Searching your location');
+			$('#search').val('Searching your location');
 
 			navigator.geolocation.getCurrentPosition(
 				function(position) {
 					$.ajax({
 						url : '/ajax/geolocation/?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude,
 						success : function(response) {
-							$('input[name=searchbox]').val(response);
+							$('#search').val(response);
 
 							yHack.search();
 						}
 					});
 				},
 				function() {
-					$('input[name=searchbox]').val('');	
+					$('#search').val('');	
 				}
 			);	
 		}
