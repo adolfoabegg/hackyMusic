abdfe4c17acc7e03a421a2e6a367b667f8d54d48
Alexander Brausewetter <alex@xoob.de>
2011-05-15 09:18:19 +0300

    JS API

--- public/js/yHack.js
+++ public/js/yHack.js
@@ -1,28 +1,77 @@
 yHack = {
 	init : function() {
-		if (navigator.geolocation) {
-			$('#search').val('Searching your location');
+        this.loadLocation();
+	},
 
+    loadLocation : function() {
+		if ($.cookie('customLocation')) {
+            $('#search').val($.cookie('customLocation'));
+            this.search();
+        } else if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
 				function(position) {
 					$.ajax({
-						url : '/ajax/geolocation/?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude,
+						url : '/ajax/geolocation',
+                        data : {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        },
 						success : function(response) {
 							$('#search').val(response);
-
-							yHack.search();
+							this.search();
 						}
 					});
 				},
 				function() {
-					$('#search').val('');	
 				}
 			);	
 		}
-	},
+    },
+
+    showSpinner : function() {
+        $('#spinner').show();
+    },
+    hideSpinner : function() {
+        $('#spinner').hide();
+    },
+
+    highlightAutocompleteItem : function(s, t) {
+        var matcher = new RegExp("(" + $.ui.autocomplete.escapeRegex(t) + ")", "ig");
+        return s.replace(matcher, "<strong>$1</strong>");
+    },
 
 	search : function() {
-	}
+        this.showSpinner();
+
+        $.ajax({
+            url : '/ajax/fetch-songs',
+            data : {
+                address: $('#search').val()
+            },
+            success : function(response) {
+                this.hideSpinner();
+            }
+        });
+	},
+
+    play : function(nextSong) {
+        var lastSong = $('.song.playing');
+        lastSong.removeClass('playing');
+
+        if (nextSong) {
+            $('#youtube').show();
+
+            if (nextSong.data('start')) {
+                player.loadVideoById(nextSong.data('video'), nextSong.data('start'));
+            } else {
+                player.loadVideoById(nextSong.data('video'));
+            }
+
+            nextSong.addClass('playing');
+        } else {
+            $('#youtube').hide();
+        }
+    }
 };
 
 
